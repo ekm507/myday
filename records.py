9cc34b8f53c9bcede1c48411f64416e96a47f15f
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2021-09-11 16:44:44 +0430

    add cryptography to all functions

--- records.py
+++ records.py
@@ -1,13 +1,13 @@
 import sqlite3
 import pickle
 import datetime
+import dataprocess
 
 class Records:
-    def __init__(self, database_filename, first_table_name, hash_salt, key) -> None:
+    def __init__(self, database_filename, first_table_name, fernet_object = None) -> None:
         self.database_filename = database_filename
         self.first_table_name = first_table_name
-        self.hash_salt = hash_salt
-        self.key = key
+        self.fernet_object = fernet_object
 
         self.connection = sqlite3.Connection(self.database_filename)
         self.cursor = self.connection.cursor()
@@ -19,19 +19,33 @@ class Records:
         self.cursor.execute(f'''CREATE TABLE IF NOT EXISTS {table_name}
                     (date datetime, list BLOB, contents BLOB)''')
 
-    def pack(self, contents_types:list, contents:list):
+    def pack(self, contents_types:list, contents:list, fernet_object=None):
 
         date_and_time = datetime.datetime.now()
         contents_types_bin = pickle.dumps(contents_types)
         contents_bin = pickle.dumps(contents)
 
-        return (date_and_time, contents_types_bin, contents_bin)
+        if fernet_object is None:
+            packed_data = (date_and_time, contents_types_bin, contents_bin)
+        
+        else:
+            contents_types_bin_crypt = fernet_object.encrypt(contents_types_bin)
+            contents_bin_crypt = fernet_object.encrypt(contents_bin)
+            packed_data = (date_and_time, contents_types_bin_crypt, contents_bin_crypt)
+
+        return packed_data
     
-    def unpack(self, package):
+    def unpack(self, package, fernet_object=None):
         if isinstance(package, (tuple, list)):
             package_unpacked = []
             for object in package:
-                object_unpacked = pickle.loads(object)
+
+                if fernet_object is None:
+                    object_unpacked = pickle.loads(object)
+                else:
+                    object_decrypted = fernet_object.decrypt(object)
+                    object_unpacked = pickle.loads(object_decrypted)
+
                 package_unpacked.append(object_unpacked)
 
         else:
@@ -43,8 +57,10 @@ class Records:
 
         if table_name is None:
             table_name = self.first_table_name
+        
+        packed_data = self.pack(contents_types, contents, fernet_object=self.fernet_object)
+        data_to_add = packed_data
 
-        data_to_add = self.pack(contents_types, contents)
         self.cursor.execute( f'''INSERT INTO {table_name} VALUES (?, ?, ?) ''', data_to_add)
         self.connection.commit()
     
@@ -57,7 +73,7 @@ class Records:
         unpacked_records = []
         for package in table_values:
             date_and_time = package[0]
-            contents, contents_types = self.unpack(package[1:])
+            contents, contents_types = self.unpack(package[1:], fernet_object=self.fernet_object)
 
             unpacked_records.append((date_and_time, contents_types, contents))
 
